1fd658fa66e3be0c81f86b7654bd1db28bba8269
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-27 23:47:17 +0530

    error solved

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { createSlice, configureStore } from "@reduxjs/toolkit";
 const initialCounterState = { counter: 0, showCounter: true };
 const counterSlice = createSlice({
   name: "counter",
-  initialCounterState,
+  initialState:initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -23,7 +23,7 @@ const counterSlice = createSlice({
 const initialAuthState = { isAuthenticated: false };
 const authSlice = createSlice({
   name: "auth",
-  initialAuthState,
+  initialState:initialAuthState,
   reducers: {
     login(state) {
       state.isAuthenticated = true;
