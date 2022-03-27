eb07634444325d5524cdff139b64fe2e80bd86df
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-27 18:59:09 +0530

    payloads in dispatching action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,12 +10,16 @@ const Counter = () => {
   const decrementhandler = () => {
     dispatch({ type: "decrement" });
   };
+  const increasehandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementhandler}>Increment</button>
+        <button onClick={increasehandler}>Increment by 5</button>
         <button onClick={decrementhandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
